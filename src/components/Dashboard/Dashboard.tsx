f93bb3e9307865a3ace51630b551f2149d4f27ce
Oleksandr Kuznietsov <funhardmode@gmail.com>
2023-02-23 17:48:14 +0200

    creating ui components

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,8 @@
 import './Dashboard.scss'
 import supportlogo from "../../assets/support.png"
+import overview from "../../assets/overview.svg";
+import templates from "../../assets/templates.svg";
+import contacts from "../../assets/contacts.svg";
 
 export const Dashboard = () => {
     return (
@@ -8,7 +11,20 @@ export const Dashboard = () => {
             <img className= "dashboard__logo" src={supportlogo} alt="" />
             <span className ="dashboard__title"> Support Helper </span>
         </div>
-        
+        <ul className="dashboard__list">
+            <li className= "dashboard__item"> 
+                <img className= "dashboard__icon" src={overview} alt="" /> 
+                <a className ="dashboard__link" href="#">Overview</a>
+            </li>
+            <li className= "dashboard__item"> 
+                <img className= "dashboard__icon" src={templates} alt="" />
+                <a className ="dashboard__link" href="#">Templates</a>
+            </li>
+            <li className= "dashboard__item"> 
+                <img className= "dashboard__icon" src={contacts} alt="" />
+                <a className ="dashboard__link" href ="#">Contacts</a>
+            </li>
+        </ul>
         </div>
     );
 }
